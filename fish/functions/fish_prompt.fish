253b5809c0ebec5e4caa0337a0c4964789d75a42
SifonFelipe <supermegafelipe@gmail.com>
2026-08-07 01:52:08 -0300

    new colors and styles

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,18 @@
 function fish_prompt
-    set last_status $status
+    set -l last_status $status
 
     # blank line
     echo
 
     # path + git branch
-    set_color 50fa7b
+    set_color 50fa7b -o  # for synthwave -> #bd93f9
     echo -n (basename (pwd))
 
     set git_branch (command git rev-parse --abbrev-ref HEAD 2>/dev/null)
     if test -n "$git_branch"
         set_color normal
         echo -n " on "
-        set_color f25aab
+        set_color f25aab -o
         echo -n " $git_branch"
     end
 
@@ -20,10 +20,10 @@ function fish_prompt
 
     # ❯ normal or red, according to status
     if test $last_status -eq 0
-        set_color 50fa7b
+        set_color 50fa7b -o
         echo -n "❯ "
     else
-        set_color red
+        set_color ff5555 -o
         echo -n "❯ "
     end
 
